074a0008e3c4a1f9bad9ca20aae4c48e01df257b
susmitha <susmitha@venkatas-mbp.stelfiber>
2023-05-13 02:08:45 +0530

    interface class

---
+++ Assignment/src/hyrtuts/Interfaceclass.java
@@ -0,0 +1,18 @@
+package hyrtuts;
+
+public interface Interfaceclass {
+	public void program();
+}
+class abc implements Interfaceclass{
+
+	
+	public void program() {
+		System.out.println("Hello world");
+		
+		
+	}
+	
+}
+
+
+
